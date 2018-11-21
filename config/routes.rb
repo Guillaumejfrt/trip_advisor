0c059a2a5d877484ed7e6423230251ed405d95cb
Guillaume Jauffret <guillaumejfrt@gmail.com>
2018-11-21 10:02:45 +0100

    beyond CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    collection do
+      get "best", to: 'restaurants#best'
+      get "worst", to: 'restaurants#worst'
+    end
+
+    member do
+      get "chef", to: 'restaurants#chef'
+    end
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
